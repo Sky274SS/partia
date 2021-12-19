95a8f51f8d43734f60a217d59e6372c06c70196e
skymirs <kadetka274@gmail.com>
2021-12-20 00:32:56 +0400

    ww

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import './startPage.css';
 const StartPage = () => {
     return (
-        <div>
-            hello world 2
+        <div className='container_start'>
+            <div>Добро пожаловать в игру! <br/> Ты готова начать?</div>
+            <button className='button'>Начать</button>
+
         </div>
     );
 };

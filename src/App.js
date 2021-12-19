5c43f0c39a0db26105a9634c0aa87a86f07abc8b
skymirs <kadetka274@gmail.com>
2021-12-20 00:16:57 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, {useState} from "react";
+import StartPage from "./pages/StartPage";
+
+
+function App() {
+
+
+    return (
+        <div>
+<StartPage/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/pages/StartPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const StartPage = () => {
+    return (
+        <div>
+            hello world
+        </div>
+    );
+};
+
+export default StartPage;

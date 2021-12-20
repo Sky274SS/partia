6928a56c78bd2973a004f0628cf58d4c85894ccf
skymirs <kadetka274@gmail.com>
2021-12-20 12:52:31 +0400

    test1

--- src/pages/FirstLoginPage.jsx
+++ src/pages/FirstLoginPage.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const FirstLoginPage = () => {
     return (
         <div className="first_login_page_container">
+            <div className='info'>Начнем нашу партию, для этого необходимо ввести логин и пароль</div>
             <div><input type="text" className='input' placeholder='login'/></div>
-
             <div><input type="password" className='input' placeholder='password'/></div>
-
+            <button className='button'>Войти</button>
 
         </div>
     );

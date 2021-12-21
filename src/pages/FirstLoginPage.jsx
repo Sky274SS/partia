5085d818e84883d0fdf5a21b2a7ccfc1d08fe47d
skymirs <kadetka274@gmail.com>
2021-12-21 13:46:41 +0400

    test2

--- src/pages/FirstLoginPage.jsx
+++ src/pages/FirstLoginPage.jsx
@@ -4,7 +4,7 @@ const FirstLoginPage = ({state,onLogin}) => {
     let login,password,trueLogin='skymirs',truePassword='skymirs'
     return (
         <div className="first_login_page_container">
-            <div className='info'>Начнем нашу партию, для этого необходимо ввести логин и пароль</div>
+            <div className='info' style={{marginTop:"150px"}}>Начнем нашу партию, для этого необходимо ввести логин и пароль</div>
             <div><input type="text" className='input' placeholder='login' onChange={e=>{login=e.target.value}} required/></div>
             <div><input type="password" className='input' placeholder='password' onChange={e=>{password=e.target.value}} required/></div>
             <button className='button' onClick={()=>{

bf8baddca4d07837320c70d46200d707d5d92a14
skymirs <kadetka274@gmail.com>
2021-12-22 21:23:37 +0400

    final

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -3,7 +3,7 @@ import './startPage.css';
 const StartPage = ({onChange,state}) => {
     return (
         <div className='container_start'>
-            <div className='info' style={{marginTop:"200px"}}>Добро пожаловать в игру! <br/> Ты готова начать?</div>
+            <div className='info' style={{marginTop:"200px"}}>Добро пожаловать в игру!<br/> Приложение предназначено для твоего телефона, на других устройствах возможна некорректная работа!<br/> Ты готова начать?</div>
             <button className='button' onClick={()=>{onChange(!state)}}>Начать</button>
 
         </div>

d9e70455d293253ff5e3de50efef5713fb95185d
skymirs <kadetka274@gmail.com>
2021-12-22 09:38:28 +0400

    final

--- src/pages/FirstLoginPage.jsx
+++ src/pages/FirstLoginPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const FirstLoginPage = ({state,onLogin}) => {
-    let login,password,trueLogin='skymirs',truePassword='071221'
+    let login,password,trueLogin='skymirs',truePassword='020599'
     return (
         <div className="first_login_page_container">
             <div className='info' style={{marginTop:"150px"}}>Начнем нашу партию, для этого необходимо ввести логин и пароль</div>
